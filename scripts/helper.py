15ff330c76f9099a8bc6cdcdaf6876cfc396b291
Thasmika Gokal <tgokal@microsoft.com>
2022-09-12 10:22:59 +0000

    add if statement for file iteration in split_data

--- scripts/helper.py
+++ scripts/helper.py
@@ -7,7 +7,6 @@ import pandas as pd
 
 def split_data(data_path, time_column_name, split_date):
 
-    data_path = data_path.split('/')[-2]
     train_data_path = os.path.join(data_path, "upload_train_data")
     inference_data_path = os.path.join(data_path, "upload_inference_data")
     os.makedirs(train_data_path, exist_ok=True)
@@ -17,16 +16,15 @@ def split_data(data_path, time_column_name, split_date):
                   if path not in (train_data_path, inference_data_path)]
 
     for file in files_list:
-        file_name = os.path.basename(file)
-        file_extension = os.path.splitext(file_name)[1].lower()
-        df = read_file(file, file_extension)
-
-        before_split_date = df[time_column_name] < split_date
-        train_df, inference_df = df[before_split_date], df[~before_split_date]
-
-        write_file(train_df, os.path.join(train_data_path, file_name), file_extension)
-        write_file(inference_df, os.path.join(inference_data_path, file_name), file_extension)
-
+        if '.csv' in file:
+            file_name = os.path.basename(file)
+            file_extension = os.path.splitext(file_name)[1].lower()
+            df = read_file(file, file_extension)
+            before_split_date = df[time_column_name] < split_date
+            train_df, inference_df = df[before_split_date], df[~before_split_date]
+            write_file(train_df, os.path.join(train_data_path, file_name), file_extension)
+            write_file(inference_df, os.path.join(inference_data_path, file_name), file_extension)
+    
     return train_data_path, inference_data_path
 
 
